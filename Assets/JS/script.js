1c1b2c20ae68d01073a1d43f863fd575c1d63270
Zach Smith <zachsmith852@gmail.com>
2023-11-12 13:50:02 -0800

    Add event listener for quiz choices

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -4,20 +4,25 @@ var quizButton = document.querySelector('#quizButton')
 var quiz = document.querySelector('.quiz')
 var questionsEl = document.querySelector('.questions')
 var choicesEl = document.querySelector('.choices')
+var nextButton = document.querySelector('.nextButton')
+
+//New variables to test
+var right = document.querySelector('.right')
+var wrong = document.querySelector('.wrong')
 
 var quizQuestions = [
     {
       // add question below
       question: "Testing this question?",
       //create an array of answers for selection buttons
-      choices: ["1", '2', '3', '4', '5'],
+      choices: ["1", "2", '3', '4', '5'],
       //input the answer from one of the selections in the array
       answer: "2"
     },
     {
-      question: "",
-      choices: [""],
-      answer: ""
+      question: "Question 2?",
+      choices: ["1", '2', '3'],
+      answer: "3"
     },
     {
       question: "",
@@ -47,20 +52,24 @@ function startQuiz() {
 
 var timeInterval;
 var time = 100;
+
 function startTime() {
+//  var timer = setInterval(timeInterval);
  timeInterval = setInterval( function() {
     timeElement.textContent = 'Time: ' + time
     time --;
-    if (time <= 0) {
-        clearInterval()
+    if (time < 0) {
+        clearInterval(timeInterval);
         // eventually write out a function call for end quiz
     }
  }, 1000);
 }
 
+var answerChoices = quizQuestions[questionIndex].choices
+
 function renderQuestions() {
  questionsEl.textContent = quizQuestions[questionIndex].question
- var answerChoices = quizQuestions[questionIndex].choices
+ 
  var choicesContainer = '';
  for (var i = 0; i < answerChoices.length; i++) {
     choicesContainer += `<li class = 'choiceStyle'>${answerChoices[i]}</li>`
@@ -68,7 +77,30 @@ function renderQuestions() {
  }
 }
 
+function questionSubmit() {
+  var choice = localStorage.getItem('choice')
+  console.log(choice);
+   if ( choice === quizQuestions[questionIndex].answer) {
+      right.classList.remove('hide');
+    } else {
+      wrong.classList.remove('hide')
+    }
+    }
+
+
+
+
+ 
+
+
   quizButton.addEventListener('click', function(event){
    event.preventDefault();
    startQuiz();
   })
+
+  choicesEl.addEventListener('click', function select(event) {
+    // event.preventDefault();
+    var choice = event.target.textContent
+    localStorage.setItem('choice', choice);
+    questionSubmit();
+  }, {once : true});
